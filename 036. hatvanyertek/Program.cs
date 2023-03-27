51fbf7538cb754738ac73935afed2f3ccaa76f4b
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-27 14:04:34 +0200

    Hatványérték kiszámolása

---
+++ 036. hatvanyertek/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _036.hatvanyertek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem a hatvány alapot! ");
+            int alap = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérem a hatvány kitevőt! ");
+            int kitevo = Convert.ToInt32(Console.ReadLine());
+
+            double pow_ab = Math.Pow(alap, kitevo);
+
+            Console.WriteLine("A hatványérték: " + pow_ab);
+
+            Console.ReadLine();
+        }
+    }
+}
